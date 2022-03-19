64c9ecb0b1e88441cc17056c87c356ca4b939d3c
Knyazev Mikhail <knyazevmo@gmail.com>
2022-03-19 16:26:50 +0300

    start project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="nav">
+      <div>
+        <a href="#s" alt="hi">
+          Profile
+        </a>
+      </div>
+      <div>
+        <a href="#s">Messages</a>
+      </div>
+      <div>
+        <a href="#s">News</a>
+      </div>
+      <div>
+        <a href="#s">Music</a>
+      </div>
+      <div>
+        <a href="#s">Settings</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img
+          src="https://avatars.mds.yandex.net/i?id=0e005b41d0a10d453ea797777f307955-5876637-images-thumbs&n=13"
+          alt="hi"
+        ></img>
+      </div>
+      <div>ava + description</div>
+      <div>
+        My Post
+        <div>New Post</div>
+        <div>
+          <div>post1</div>
+          <div>post2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
