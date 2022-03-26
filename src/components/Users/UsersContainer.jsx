115d812fde7a301d940b95ca4adcc7c240747fd2
Knyazev Mikhail <knyazevmo@gmail.com>
2022-03-26 18:56:38 +0300

    refactoring. add preloader

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 import { connect } from "react-redux";
-import { followAC, setTotalUsersCountAC, setUsersAC } from "../../redux/users-reducer";
+import {
+  follow,
+  setTotalUsersCount,
+  setUsers,
+  toggleIsLoading,
+} from "../../redux/users-reducer";
 import Users from "./Users";
-import { setCurrentPageAC } from './../../redux/users-reducer';
+import { setCurrentPage } from "./../../redux/users-reducer";
 import axios from "axios";
+import Preloader from "../common/Preloader/Preloader";
 
 class UsersContainer extends React.Component {
   componentDidMount() {
+    this.props.toggleIsLoading(true);
     axios
       .get(
         `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`
       )
       .then((response) => {
+        this.props.toggleIsLoading(false);
         console.log(response);
         // debugger;
         this.props.setUsers(response.data.items);
@@ -21,11 +29,13 @@ class UsersContainer extends React.Component {
 
   onPageChanged = (pageNumber) => {
     this.props.setCurrentPage(pageNumber);
+    this.props.toggleIsLoading(true);
     axios
       .get(
         `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count${this.props.pageSize}`
       )
       .then((response) => {
+        this.props.toggleIsLoading(false);
         console.log(response);
         // debugger;
         this.props.setUsers(response.data.items);
@@ -35,15 +45,19 @@ class UsersContainer extends React.Component {
   render() {
     // debugger;
     return (
-      <Users
-        totalUsersCount={this.props.totalUsersCount}
-        pageSize={this.props.pageSize}
-        onPageChanged={this.onPageChanged}
-        currentPage={this.props.currentPage}
-        // pages={this.props.pages}
-        users={this.props.users}
-        toggleFollowed={this.props.toggleFollowed}
-      />
+      <>
+        {this.props.isLoading ? <Preloader /> : null}
+        <Users
+          totalUsersCount={this.props.totalUsersCount}
+          pageSize={this.props.pageSize}
+          onPageChanged={this.onPageChanged}
+          currentPage={this.props.currentPage}
+          // pages={this.props.pages}
+          users={this.props.users}
+          toggleFollowed={this.props.toggleFollowed}
+          // isLoading={this.props.isLoading}
+        />
+      </>
     );
   }
 }
@@ -54,24 +68,35 @@ let mapStateToProps = (state) => {
     pageSize: state.usersPage.pageSize,
     totalUsersCount: state.usersPage.totalUsersCount,
     currentPage: state.usersPage.currentPage,
+    isLoading: state.usersPage.isLoading,
   };
 };
 
-let mapDispatchToProps = (dispatch) => {
-  return {
-    toggleFollowed: (userId) => {
-      dispatch(followAC(userId));
-    },
-    setUsers: (users) => {
-      dispatch(setUsersAC(users));
-    },
-    setCurrentPage: (pageNumber) => {
-      dispatch(setCurrentPageAC(pageNumber));
-    },
-    setTotalUsersCount: (totalCount) => {
-      dispatch(setTotalUsersCountAC(totalCount));
-    }
-  };
-};
+// let mapDispatchToProps = (dispatch) => {
+//   return {
+//     toggleFollowed: (userId) => {
+//       dispatch(followAC(userId));
+//     },
+//     setUsers: (users) => {
+//       dispatch(setUsersAC(users));
+//     },
+//     setCurrentPage: (pageNumber) => {
+//       dispatch(setCurrentPageAC(pageNumber));
+//     },
+//     setTotalUsersCount: (totalCount) => {
+//       dispatch(setTotalUsersCountAC(totalCount));
+//     },
+//     toggleIsLoading: (isLoading) => {
+//       dispatch(toggleIsLoadingAC(isLoading));
+//     }
 
-export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
+//   };
+// };
+
+export default connect(mapStateToProps, {
+  follow,
+  setUsers,
+  setCurrentPage,
+  setTotalUsersCount,
+  toggleIsLoading,
+})(UsersContainer);
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -2,13 +2,14 @@ const TOGGLE_FOLLOW = "TOGGLE-FOLLOW";
 const SET_USERS = "SET-USERS";
 const SET_CURRENT_PAGE = "SET-CURRENT-PAGE";
 const SET_TOTAL_USERSCOUNT = "SET-TOTAL-USERSCOUNT";
+const TOGGLE_IS_LOADING = "TOGGLE-IS-LOADING";
 
 let initialState = {
     users: [],
-    pageSize: 50,
+    pageSize: 10,
     totalUsersCount: 0,
-    currentPage: 1
-
+    currentPage: 1,
+    isLoading: false
 }
 
 const usersReducer = (state = initialState, action) => {
@@ -41,14 +42,21 @@ const usersReducer = (state = initialState, action) => {
                 totalUsersCount: action.count
             }
         }
+        case TOGGLE_IS_LOADING: {
+            return {
+                ...state,
+                isLoading: action.isLoading
+            }
+        }
         default:
             return state;
     }
 }
 
-export const followAC = (userId) => ({ type: TOGGLE_FOLLOW, userId })
-export const setUsersAC = (users) => ({ type: SET_USERS, users })
-export const setCurrentPageAC = (currentPage) => ({type: SET_CURRENT_PAGE, currentPage})
-export const setTotalUsersCountAC = (totalCount) => ({type: SET_TOTAL_USERSCOUNT, count: totalCount})
+export const follow = (userId) => ({ type: TOGGLE_FOLLOW, userId })
+export const setUsers = (users) => ({ type: SET_USERS, users })
+export const setCurrentPage = (currentPage) => ({type: SET_CURRENT_PAGE, currentPage})
+export const setTotalUsersCount = (totalCount) => ({type: SET_TOTAL_USERSCOUNT, count: totalCount})
+export const toggleIsLoading = (isLoading) => ({type: TOGGLE_IS_LOADING, isLoading})
 
 export default usersReducer;
